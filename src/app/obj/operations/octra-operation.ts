13770dc4bb864a66be177efc8648ef777dd34d11
julianpoemp <j.poemp@campus.lmu.de>
2019-07-04 11:24:38 +0200

    changed logos

--- src/app/obj/operations/octra-operation.ts
+++ src/app/obj/operations/octra-operation.ts
@@ -87,7 +87,7 @@ export class OCTRAOperation extends ToolOperation {
       const audio = `audio=${encodeURIComponent((<UploadOperation> this.operations[0]).wavFile.url)}`;
       let transcript = `transcript=`;
       const embedded = `embedded=1`;
-      const host = `host=${encodeURIComponent(AppSettings.getLanguageByCode(this.task.language, this.task.operations[1].providerInformation.provider).host)}`;
+      const host = `host=${encodeURIComponent(AppSettings.getLanguageByCode(this.task.language, this.task.asr).host)}`;
 
 
       if (this.results.length < 1) {
