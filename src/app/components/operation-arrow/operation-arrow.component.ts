e6278bc5caf402ca984db645e2e4ee4f3d9d3af4
julianpoemp <j.poemp@campus.lmu.de>
2019-07-07 11:27:01 +0200

    fixed blue arrow

--- src/app/components/operation-arrow/operation-arrow.component.ts
+++ src/app/components/operation-arrow/operation-arrow.component.ts
@@ -1,22 +1,12 @@
-import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-operation-arrow',
   templateUrl: './operation-arrow.component.html',
   styleUrls: ['./operation-arrow.component.css']
 })
-export class OperationArrowComponent implements OnInit, AfterViewInit {
-
-  public size = {
-    width: 50,
-    height: 20
-  };
-
-  public settings = {
-    color: 'cornflowerblue'
-  };
-
-  @ViewChild('outer', { static: true }) outer;
+export class OperationArrowComponent implements OnInit {
+  @ViewChild('outer', {static: true}) outer;
   @Input() type: 'arrow' | 'circle' = 'arrow';
   @Input() first = false;
 
@@ -24,21 +14,5 @@ export class OperationArrowComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    this.size.width = this.outer.nativeElement.offsetWidth;
-    setInterval(() => {
-      this.size.width = this.outer.nativeElement.offsetWidth;
-    }, 300);
-  }
-
-  ngAfterViewInit() {
-    this.size.width = this.outer.nativeElement.offsetWidth;
-  }
-
-  public getTriangle() {
-    return `${this.size.width - 10},0 ${this.size.width},5 ${this.size.width - 10},10`;
-  }
-
-  onResize($event) {
-    this.size.width = this.outer.nativeElement.offsetWidth;
   }
 }
