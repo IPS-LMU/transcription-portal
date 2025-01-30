8aea11cfe022a1b537f35f340e63aef7017c9571
Julian Pömp <jp@poemp.net>
2025-01-30 18:06:46 +0100

    fix(tportal): rows are not highlighted on selection

--- apps/transcription-portal/src/app/components/proceedings/directives/proceedings-row.directive.ts
+++ apps/transcription-portal/src/app/components/proceedings/directives/proceedings-row.directive.ts
@@ -1,15 +1,22 @@
-import {AfterViewInit, Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges} from '@angular/core';
-import {Operation} from '../../../obj/operations/operation';
-import {Task, TaskDirectory} from '../../../obj/tasks';
-import {hasProperty} from '@octra/utilities';
+import {
+  AfterViewInit,
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges,
+  Renderer2,
+  SimpleChanges,
+} from '@angular/core';
+import { Operation } from '../../../obj/operations/operation';
+import { Task, TaskDirectory } from '../../../obj/tasks';
+import { hasProperty } from '@octra/utilities';
 
 @Directive({
-    selector: '[tportalProceedingsRow]',
-    standalone: true
+  selector: '[tportalProceedingsRow]',
+  standalone: true,
 })
 export class ProceedingsRowDirective implements OnChanges, AfterViewInit {
-
-  @Input() entry?: (Task | TaskDirectory);
+  @Input() entry?: Task | TaskDirectory;
   @Input() toolSelectedOperation?: Operation;
   @Input() rowSelected = false;
 
@@ -18,9 +25,11 @@ export class ProceedingsRowDirective implements OnChanges, AfterViewInit {
   }
 
   private get toolSelected(): boolean {
-    return ((this.toolSelectedOperation !== undefined
-        && this.toolSelectedOperation.task !== undefined)
-      && this.toolSelectedOperation.task.id === this.entry?.id);
+    return (
+      this.toolSelectedOperation !== undefined &&
+      this.toolSelectedOperation.task !== undefined &&
+      this.toolSelectedOperation.task.id === this.entry?.id
+    );
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -28,15 +37,24 @@ export class ProceedingsRowDirective implements OnChanges, AfterViewInit {
       if (this.toolSelected) {
         this.renderer.addClass(this.elementRef.nativeElement, 'tool-selected');
       } else {
-        this.renderer.removeClass(this.elementRef.nativeElement, 'tool-selected');
+        this.renderer.removeClass(
+          this.elementRef.nativeElement,
+          'tool-selected'
+        );
       }
     }
 
-    if (hasProperty(changes, 'rowSelected') && changes['rowSelected'].currentValue !== undefined) {
+    if (
+      hasProperty(changes, 'rowSelected') &&
+      changes['rowSelected'].currentValue !== undefined
+    ) {
       if (this.rowSelected) {
-        this.renderer.addClass(this.elementRef.nativeElement, 'selected');
+        this.renderer.addClass(this.elementRef.nativeElement, 'row-selected');
       } else {
-        this.renderer.removeClass(this.elementRef.nativeElement, 'selected');
+        this.renderer.removeClass(
+          this.elementRef.nativeElement,
+          'row-selected'
+        );
       }
     }
   }
@@ -44,7 +62,6 @@ export class ProceedingsRowDirective implements OnChanges, AfterViewInit {
   ngAfterViewInit() {
     if (!(this.entry === null || this.entry === undefined)) {
       // entry set
-
     } else {
       throw new Error('ProceedingsRowDirective error: no entry set');
     }
