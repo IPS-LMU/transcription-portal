eb3853fa53d2e5023b0db4999af4b1314f9016d6
Julian Pömp <jp@poemp.net>
2025-11-11 11:35:03 +0100

    fix(transcription-portal): Tportal can't load i18n file on production

--- apps/transcription-portal/src/transloco-http-loader.ts
+++ apps/transcription-portal/src/transloco-http-loader.ts
@@ -1,12 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Translation, TranslocoLoader } from '@jsverse/transloco';
-import { HttpClient } from '@angular/common/http';
+import { getBaseHrefURL, joinURL } from '@octra/utilities';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(joinURL(getBaseHrefURL(), `assets/i18n/${lang}.json`));
   }
 }
