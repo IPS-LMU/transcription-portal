32c5b9bc2ffc182acdedb7c6e711f53f62e51f3a
Julian Pömp <jp@poemp.net>
2026-04-24 10:43:02 +0200

    fix(transcription-portal): statistics not loading

--- apps/transcription-portal/src/app/modals/statistics-modal/statistics-modal.component.ts
+++ apps/transcription-portal/src/app/modals/statistics-modal/statistics-modal.component.ts
@@ -1,14 +1,14 @@
-import { AfterViewInit, Component, OnDestroy, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 import { SubscriberComponent } from '@octra/ngx-utilities';
 import { ChartConfiguration } from 'chart.js';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { BaseChartDirective } from 'ng2-charts';
-import { timer } from 'rxjs';
 import { StatisticsService } from '../../shared/statistics.service';
 import { ModeStoreService } from '../../store';
 import { AsyncPipe } from '@angular/common';
 import { TranslocoPipe } from '@jsverse/transloco';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'tportal-statistics',
@@ -16,11 +16,13 @@ import { TranslocoPipe } from '@jsverse/transloco';
   styleUrls: ['./statistics-modal.component.scss'],
   providers: [StatisticsService],
   imports: [NgCircleProgressModule, BaseChartDirective, AsyncPipe, TranslocoPipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StatisticsModalComponent extends SubscriberComponent implements OnDestroy, AfterViewInit {
+export class StatisticsModalComponent extends SubscriberComponent implements OnDestroy, OnInit {
   statisticsService = inject(StatisticsService);
   protected activeModal = inject(NgbActiveModal);
   protected modeStoreService = inject(ModeStoreService);
+  protected cd = inject(ChangeDetectorRef);
 
   public static options: NgbModalOptions = {
     size: 'xl',
@@ -48,11 +50,12 @@ export class StatisticsModalComponent extends SubscriberComponent implements OnD
     this.statisticsService.destroy();
   }
 
-  ngAfterViewInit() {
+  ngOnInit() {
     this.modeStoreService.updateProtocol();
-    this.subscribe(timer(1000), {
+    this.subscribe(interval(1000), {
       next: () => {
         this.showCharts = true;
+        this.cd.markForCheck();
       },
     });
   }
